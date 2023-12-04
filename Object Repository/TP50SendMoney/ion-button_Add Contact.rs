5c40e98967858f571a9884a3ca53742541059bf3
João Silva <2211429@my.ipleiria.pt>
2023-12-04 20:12:21 +0000

    TP-50 Add Objects

---
+++ Object Repository/TP50SendMoney/ion-button_Add Contact.rs
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ion-button_Add Contact</name>
+   <tag></tag>
+   <elementGuidId>1e2c7f48-fda2-4b54-b289-fc841f3e2182</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//ion-modal[@id='ion-overlay-2']/div/ion-content/div/ion-row/ion-card/ion-card-content/ion-button</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>ion-row.md.ion-justify-content-center > ion-card.md > ion-card-content.md.card-content-md > ion-button.md.button.button-solid.ion-activatable.ion-focusable</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>ion-button</value>
+      <webElementGuid>2ff1838f-df0c-406c-9df8-c2ac8e0b4a79</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>md button button-solid ion-activatable ion-focusable</value>
+      <webElementGuid>0fa5e3b5-b237-4a6c-b8ee-87b5c6111431</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Add Contact </value>
+      <webElementGuid>dc86f9b3-5bd6-4253-96b9-12118bbebd56</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;ion-overlay-2&quot;)/div[@class=&quot;ion-page&quot;]/ion-content[@class=&quot;ion-padding md content-ltr&quot;]/div[@class=&quot;d-flex flex-column justify-content-space-between h-100&quot;]/ion-row[@class=&quot;md ion-justify-content-center&quot;]/ion-card[@class=&quot;md&quot;]/ion-card-content[@class=&quot;md card-content-md&quot;]/ion-button[@class=&quot;md button button-solid ion-activatable ion-focusable&quot;]</value>
+      <webElementGuid>9e2646b5-e155-4602-a4d0-3a4232db7a86</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//ion-modal[@id='ion-overlay-2']/div/ion-content/div/ion-row/ion-card/ion-card-content/ion-button</value>
+      <webElementGuid>406923eb-ab03-456d-bb80-4c08504d5ff5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Save contact in my contacts list'])[1]/following::ion-button[1]</value>
+      <webElementGuid>6f5da2e8-f623-4d09-9181-304985eb8406</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Name (optional)'])[1]/following::ion-button[1]</value>
+      <webElementGuid>e047183b-9ff3-418f-9ccc-dc2a43d1b850</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Capture object:'])[1]/preceding::ion-button[1]</value>
+      <webElementGuid>cdb30a45-aeb4-4f4c-9888-a0645d2bb86c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//ion-row/ion-card/ion-card-content/ion-button</value>
+      <webElementGuid>a73db391-82aa-433b-94e8-316aefd0906e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//ion-button[(text() = 'Add Contact ' or . = 'Add Contact ')]</value>
+      <webElementGuid>980ed72e-d427-4ef1-8eef-7be612fce2d7</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/TP50SendMoney/p-modal-success-sent-to.rs
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>p-modal-success-sent-to</name>
+   <tag></tag>
+   <elementGuidId>5a2baea2-266b-48d7-95e4-ad42859d7a68</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//ion-modal[@id='ion-overlay-2']/div/ion-content/ion-card/ion-card-content/p</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>ion-card-content.md.card-content-md > p</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>p</value>
+      <webElementGuid>8983e75e-a2f2-4a02-b654-55cc2346082d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>0,01€ sent to Miguel Pedrosa Alberto</value>
+      <webElementGuid>1b4b8bdb-c208-4cb0-b79e-4494eec29057</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;ion-overlay-2&quot;)/div[@class=&quot;ion-delegate-host ion-page&quot;]/ion-content[@class=&quot;ion-text-center md content-ltr&quot;]/ion-card[@class=&quot;md&quot;]/ion-card-content[@class=&quot;md card-content-md&quot;]/p[1]</value>
+      <webElementGuid>6edbd94f-39be-4a52-9a81-e9454420921f</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//ion-modal[@id='ion-overlay-2']/div/ion-content/ion-card/ion-card-content/p</value>
+      <webElementGuid>c9dd41e0-f3fe-40eb-a565-bbf0a38e555f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Send'])[1]/following::p[1]</value>
+      <webElementGuid>cb4d3275-0dab-47e4-a502-c8ab4aa979c8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Confirmation Code'])[1]/following::p[1]</value>
+      <webElementGuid>79d1d3fb-2f71-4813-ac38-68b078371d55</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Continue'])[1]/preceding::p[1]</value>
+      <webElementGuid>bc468d28-8800-45e0-8958-00d60ee8ebaa</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Capture object:'])[1]/preceding::p[1]</value>
+      <webElementGuid>57e15158-d9ce-4c0d-b00c-912fc39b1acc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='0,01€ sent to Miguel Pedrosa Alberto']/parent::*</value>
+      <webElementGuid>c633124d-0d6c-4b15-9df8-6c76f3a0dfc3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//ion-card-content/p</value>
+      <webElementGuid>3f2df398-080c-4fb0-9f6c-87c05f42483f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//p[(text() = '0,01€ sent to Miguel Pedrosa Alberto' or . = '0,01€ sent to Miguel Pedrosa Alberto')]</value>
+      <webElementGuid>86f00319-9fd1-46a9-be27-7e446e8b1dcb</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
